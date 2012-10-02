8027099ac5cac921d092ead82e989531032e1af3
Remco Wendt <remco@maykinmedia.nl>
2012-10-02 09:57:17 +0200

    Added actual jquery-json files and prepped for release

--- js/jquery_json/__init__.py
+++ js/jquery_json/__init__.py
@@ -1,7 +1,10 @@
+from js.jquery import jquery
+
 from fanstatic import Library, Resource
+
 
 library = Library('jquery-json', 'resources')
 
-# Define the resources in the library like this.
-# For options and examples, see the fanstatic documentation.
-# resource1 = Resource(library, 'style.css')
+jquery_json = Resource(library, 'jquery.json.js',
+    minified='jquery.json.min.js',
+    depends=[jquery])
